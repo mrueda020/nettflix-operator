ef3ed7af17a4a4e865a037e8644e03d1432a5407
mrueda020 <mrueda020@hotmail.com>
2023-06-06 22:26:39 -0600

    Implement controller

--- internal/controller/netflix_controller.go
+++ internal/controller/netflix_controller.go
@@ -19,12 +19,16 @@ package controller
 import (
 	"context"
 
+	webv1alpha1 "github.com/mrueda020/netflix-operator/api/v1alpha1"
+	"github.com/mrueda020/netflix-operator/assets"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	webv1alpha1 "github.com/mrueda020/netflix-operator/api/v1alpha1"
 )
 
 // NetflixReconciler reconciles a Netflix object
@@ -47,9 +51,61 @@ type NetflixReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *NetflixReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
+	netflixOperatorCR := &webv1alpha1.Netflix{}
+	err := r.Get(ctx, req.NamespacedName, netflixOperatorCR)
 
-	// TODO(user): your logic here
+	if err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("CR is not found for netflix operator")
+			return ctrl.Result{}, nil
+		}
+		logger.Error(err, "Error getting operator resource object")
+		return ctrl.Result{}, err
+	}
+
+	deployment := &appsv1.Deployment{}
+	err = r.Get(ctx, req.NamespacedName, deployment)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("No deployments for netflix, creating deployment")
+			deployment.Namespace = req.Namespace
+			deployment.Name = req.Name
+
+			deploymentManifest, _ := assets.GetDeploymentFromFile("manifests/netflix_deployment.yaml")
+			deploymentManifest.Spec.Replicas = pointer.Int32(3)
+
+			err = r.Create(ctx, deploymentManifest)
+			if err != nil {
+				logger.Error(err, "Error creating netflix deployment.")
+				return ctrl.Result{}, err
+			}
+		}
+		logger.Error(err, "Error getting netflix deployment.")
+		return ctrl.Result{}, err
+	}
+
+	service := corev1.Service{}
+	err = r.Get(ctx, req.NamespacedName, &service)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("No deployments for netflix, creating deployment")
+			service.Namespace = req.Namespace
+			service.Name = req.Name
+
+			serviceManifest, _ := assets.GetDeploymentFromFile("manifests/netflix_deployment.yaml")
+			//serviceManifest.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort = *netflixOperatorCR.Spec.Port
+
+			err = r.Create(ctx, serviceManifest)
+			if err != nil {
+				logger.Error(err, "Error creating netflix service.")
+				return ctrl.Result{}, err
+			}
+		}
+		logger.Error(err, "Error getting netflix service.")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
